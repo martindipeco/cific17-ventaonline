da64cb022ca1c4c01dfa2561f58ebd56f147174f
martindipeco <martindipeco@gmail.com>
2024-09-28 15:13:10 -0300

    todo el main comentado, se esta entrando directo por menu

--- src/Main.java
+++ src/Main.java
@@ -15,51 +15,51 @@ public class Main {
 
         Menu menu = new Menu();
 
-        //repos simulados, A REEMPLAZAR POR BASE DE DATOS
-        ProductoRepositorio productoRepositorio = new ProductoRepositorio();
-        UsuarioRepositorio usuarioRepositorio = new UsuarioRepositorio();
-        PedidoRepositorio pedidoRepositorio = new PedidoRepositorio();
-
-        //inicializar servicios
-        ProductoServicio productoServicio = new ProductoServicio(productoRepositorio);
-        CarritoServicio carritoServicio = new CarritoServicio();
-        PedidoServicio pedidoServicio = new PedidoServicio();
-        pedidoServicio.setPedidoRepositorio(pedidoRepositorio);
-
-        //CASO DE USO 1: instancio carrito con usuario logueado
-        Carrito carrito1 = new Carrito(usuarioRepositorio.getListaDeUsuarios().get(0));
-
-        //proceso de compra
-        carritoServicio.agregarProducto(carrito1, productoRepositorio.getListaDeProductos().get(0), 1);
-        carritoServicio.mostrarCarrito(carrito1);
-        carritoServicio.agregarProducto(carrito1, productoRepositorio.getListaDeProductos().get(
-                productoRepositorio.getListaDeProductos().size()-1), 2);
-        carritoServicio.mostrarCarrito(carrito1);
-
-        //se confirma la compra
-        Pedido pedido1 = new Pedido(carrito1);
-
-        //se informa la compra
-        System.out.println("Pedido confirmado: ");
-        pedidoServicio.mostrarPedido(pedido1);
-
-        //se agrega el pedido al repo
-        pedidoServicio.agregarPedido(pedido1);
-
-        //CASO DE USO 2: Compra sin usuario logueado
-        Carrito carritoAnonimo = new Carrito();
-
-        carritoServicio.agregarProducto(carritoAnonimo, productoRepositorio.getListaDeProductos().get(1), 3);
-        carritoServicio.agregarProducto(carritoAnonimo, productoRepositorio.getListaDeProductos().get(2), 2);
-        carritoServicio.mostrarCarrito(carritoAnonimo);
-
-        Pedido pedido2 = new Pedido(carritoAnonimo);
-
-        pedidoServicio.agregarPedido(pedido2);
-        pedidoServicio.mostrarPedido(pedido2);
-
-        //historial de pedidos
-        //System.out.println(pedidoServicio.getListaPedidos());
+//        //repos simulados, A REEMPLAZAR POR BASE DE DATOS
+//        ProductoRepositorio productoRepositorio = new ProductoRepositorio();
+//        UsuarioRepositorio usuarioRepositorio = new UsuarioRepositorio();
+//        PedidoRepositorio pedidoRepositorio = new PedidoRepositorio();
+//
+//        //inicializar servicios
+//        ProductoServicio productoServicio = new ProductoServicio(productoRepositorio);
+//        CarritoServicio carritoServicio = new CarritoServicio();
+//        PedidoServicio pedidoServicio = new PedidoServicio();
+//        pedidoServicio.setPedidoRepositorio(pedidoRepositorio);
+//
+//        //CASO DE USO 1: instancio carrito con usuario logueado
+//        Carrito carrito1 = new Carrito(usuarioRepositorio.getListaDeUsuarios().get(0));
+//
+//        //proceso de compra
+//        carritoServicio.agregarProducto(carrito1, productoRepositorio.getListaDeProductos().get(0), 1);
+//        carritoServicio.mostrarCarrito(carrito1);
+//        carritoServicio.agregarProducto(carrito1, productoRepositorio.getListaDeProductos().get(
+//                productoRepositorio.getListaDeProductos().size()-1), 2);
+//        carritoServicio.mostrarCarrito(carrito1);
+//
+//        //se confirma la compra
+//        Pedido pedido1 = new Pedido(carrito1);
+//
+//        //se informa la compra
+//        System.out.println("Pedido confirmado: ");
+//        pedidoServicio.mostrarPedido(pedido1);
+//
+//        //se agrega el pedido al repo
+//        pedidoServicio.agregarPedido(pedido1);
+//
+//        //CASO DE USO 2: Compra sin usuario logueado
+//        Carrito carritoAnonimo = new Carrito();
+//
+//        carritoServicio.agregarProducto(carritoAnonimo, productoRepositorio.getListaDeProductos().get(1), 3);
+//        carritoServicio.agregarProducto(carritoAnonimo, productoRepositorio.getListaDeProductos().get(2), 2);
+//        carritoServicio.mostrarCarrito(carritoAnonimo);
+//
+//        Pedido pedido2 = new Pedido(carritoAnonimo);
+//
+//        pedidoServicio.agregarPedido(pedido2);
+//        pedidoServicio.mostrarPedido(pedido2);
+//
+//        //historial de pedidos
+//        //System.out.println(pedidoServicio.getListaPedidos());
 
         menu.muestraMenu();
 
